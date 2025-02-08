0059bb950dfea011afb107cf4b0dd2ec18326aae
Andrey <andreyka.ershov.2015@mail.ru>
2025-02-08 19:33:29 +0300

    Update

---
+++ Assets/_Project/Scripts/Game/Gameplay/Environment/HumanAnimation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace _Project.Gameplay
+{
+    public class HumanAnimation : MonoBehaviour
+    {
+        private const string Randomizer = nameof(Randomizer);
+        
+        [SerializeField] private Animator _animator;
+
+        private void Awake()
+        {
+            int randomValue = Random.Range(1, 7);
+            _animator.SetInteger(Randomizer, randomValue);
+        }
+    }
+}
